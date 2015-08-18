545c6c945ddc515bb6d602f5464d1d267f92c92b
RoseTHERESA <rose.theresa@title9music.org>
2015-08-17 22:24:13 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,48 +1,195 @@
-// Your JS goes here
+// PIXEL ART with COLOR TILES
 
-for (var i=0; i<100; i++) {
-	var gridDiv = document.createElement("div");
-	gridDiv.style.width = "1.7%";
-	gridDiv.style.paddingBottom = "1.7%";
-	gridDiv.style.paddingBottom.color = "grey";
-	gridDiv.style.float = "left";
-	gridDiv.style.border = "2px solid grey";
-	gridDiv.style.backgroundColor = "white";
-	//if (i %2 === 0) newDiv.style.backgroundColor = "red";
-	//	else newDiv.style.backgroundColor = "black";
-	document.body.appendChild(gridDiv);
-};
+// Create the grid
 
-/*
-var body = document.getElementsByTagName('body')[0];
-var newDiv;
-var color = "white";
+for (var i=0; i < 1012; i++) {
+	var body = document.querySelector("body");
+	var tile = document.createElement("div");
+		body.appendChild(tile);
+		tile.style.width = "2%";
+		tile.style.float = "left";
+		tile.style.paddingBottom = "2%";
+		tile.style.border = "1px solid grey";
+		tile.style.margin = "0.5px";
 
-for(var i = 0; i < 500; i++){
-    newDiv = document.createElement('DIV');
-    body.appendChild(newDiv);
-    newDiv.style.backgroundColor = color;
-    newDiv.style.width = "1%";
-    newDiv.style.float = "left";
-    newDiv.style.border = "1px solid black";
-    newDiv.style.paddingBottom = "1%";
-    newDiv.addEventListener("click", function(){
-        this.style.backgroundColor = color;
-    })
-}
+	}
 
-var colors = ["red", "orange", "yellow", "green", "blue"];
-colors.forEach(function(anything){
-    var div = document.createElement("DIV");
-    body.appendChild(div);
-    div.style.width = "200px";
-    div.style.border = "1px solid black";
-    div.style.backgroundColor = anything;
-    div.style.paddingBottom = "10%"
+// Create color palette
+// http://www.crockford.com/wrrrld/color.html 
+    var colors = [
+            "aliceblue",
+            "antiquewhite",
+            "aqua",
+            "aquamarine",
+            "azure",
+            "beige",
+            "bisque",
+            "black",
+            "blanchedalmond",
+            "blue",
+            "blueviolet",
+            "brown",
+            "burlywood",
+            "cadetblue",
+            "chartreuse",
+            "chocolate",
+            "coral",
+            "cornflowerblue",
+            "cornsilk",
+            "crimson",
+            "cyan",
+            "darkblue",
+            "darkcyan",
+            "darkgoldenrod",
+            "darkgray",
+            "darkgreen",
+            "darkkhaki",
+            "darkmagenta",
+            "darkolivegreen",
+            "darkorange",
+            "darkorchid",
+            "darkred",
+            "darksalmon",
+            "darkseagreen",
+            "darkslateblue",
+            "darkslategray",
+            "darkturquoise",
+            "darkviolet",
+            "deeppink",
+            "deepskyblue",
+            "dimgray",
+            "dodgerblue",
+            "firebrick",
+            "floralwhite",
+            "forestgreen",
+            "fuchsia",
+            "gainsboro",
+            "ghostwhite",
+            "gold",
+            "goldenrod",
+            "gray",
+            "green",
+            "greenyellow",
+            "honeydew",
+            "hotpink",
+            "indianred",
+            "indigo",
+            "ivory",
+            "khaki",
+            "lavender",
+            "lavenderblush",
+            "lawngreen",
+            "lemonchiffon",
+            "lightblue",
+            "lightcoral",
+            "lightcyan",
+            "lightgoldenrodyellow",
+            "lightgray",
+            "lightgreen",
+            "lightpink",
+            "lightsalmon",
+            "lightseagreen",
+            "lightskyblue",
+            "lightslategray",
+            "lightsteelblue",
+            "lightyellow",
+            "lime",
+            "limegreen",
+            "linen",
+            "magenta",
+            "maroon",
+            "mediumaquamarine",
+            "mediumblue",
+            "mediumorchid",
+            "mediumpurple",
+            "mediumseagreen",
+            "mediumslateblue",
+            "mediumspringgreen",
+            "mediumturquoise",
+            "mediumvioletred",
+            "midnightblue",
+            "mintcream",
+            "mistyrose",
+            "moccasin",
+            "navajowhite",
+            "navy",
+            "oldlace",
+            "olive",
+            "olivedrab",
+            "orange",
+            "orangered",
+            "orchid",
+            "palegoldenrod",
+            "palegreen",
+            "paleturquoise",
+            "palevioletred",
+            "papayawhip",
+            "peachpuff",
+            "peru",
+            "pink",
+            "plum",
+            "powderblue",
+            "purple",
+            "rebeccapurple",
+            "red",
+            "rosybrown",
+            "royalblue",
+            "saddlebrown",
+            "salmon",
+            "sandybrown",
+            "seagreen",
+            "seashell",
+            "sienna",
+            "silver",
+            "skyblue",
+            "slateblue",
+            "slategray",
+            "snow",
+            "springgreen",
+            "steelblue",
+            "tan",
+            "teal",
+            "thistle",
+            "tomato",
+            "turquoise",
+            "violet",
+            "wheat",
+            "white",
+            "whitesmoke",
+            "yellow",
+            "yellowgreen"
+        ];
 
-    div.addEventListener("click", function(){
-        color = this.style.backgroundColor;
-    });
-})
-*/
+		colors.forEach(function(color) {
+			var newDiv = document.createElement("DIV");
+				body.appendChild(newDiv);
+				newDiv.style.width = "2%";
+				newDiv.style.float = "left";
+				newDiv.style.paddingBottom = "2%";
+				newDiv.style.border = "1px solid grey";
+				newDiv.style.margin = "0.5px";
+				newDiv.style.backgroundColor = color;
 
+			newDiv.addEventListener("click", function() {
+				currentColor = this.style.backgroundColor;
+			});
+
+		})
+
+
+
+
+// Listen for clicks
+
+	var divs = document.querySelectorAll("div");
+
+
+		for(var i=0; i < divs.length; i++) {
+			divs[i].addEventListener("click", function() {
+			this.style.backgroundColor = currentColor;
+		});	
+
+			divs[i].addEventListener("dragenter", function() {
+			this.style.backgroundColor = currentColor;
+		});
+	} 
